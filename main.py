45f69f4b573197d107c0f357cc1971693fa77343
Damian <damian.jahn@icloud.com>
2020-10-20 20:07:59 +0200

    Configuring the graph

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ years = [1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000,
 
 pops = [2.5, 2.7, 3, 3.3, 3.6, 4.0, 4.4, 4.8, 5.3, 5.7, 6.1, 6.5, 6.9, 7.3]
 
+deaths = [1.2, 1.7, 1.8, 2.2, 2.5, 2.7, 2.9, 3, 3.1, 3.3, 3.5, 3.8, 4.0, 4.3]
+
+lines = plt.plot(years, pops, years, deaths)
+plt.grid(True)
+
+plt.setp(lines, color=(1, .4, .4), marker="o")
+
+plt.show()
+
 # World population graph
-plt.plot(years, pops, color=(255/255, 100/255, 100/255))
+plt.plot(years, pops, '--', color=(255/255, 100/255, 100/255))
+plt.plot(years, deaths, color=(.6, .6, 1))
 
 plt.ylabel("Population in billions")
 plt.xlabel("Population growth by year")
