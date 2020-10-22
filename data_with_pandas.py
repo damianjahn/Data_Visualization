465f45e4a0bd59269cfdcf4ab4eddd032b7fb937
Damian <damian.jahn@icloud.com>
2020-10-22 13:55:37 +0200

    Using Panda's data for pie charts

--- data_with_pandas.py
+++ data_with_pandas.py
@@ -10,4 +10,12 @@ df = pd.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'march_ir'])
 
 df['total_ir'] = df['jan_ir'] + df['feb_ir'] + df['march_ir']
 
-print(df)
+color = [(1, .4, .4), (1, .6, 1), (.5, .3, 1), (.3, 1, .5), (.7, .7, .2)]
+
+plt.pie(df['total_ir'],
+        labels=df['names'],
+        colors=color,
+        autopct='%1.1f%%')
+plt.axis('equal')
+
+plt.show()
