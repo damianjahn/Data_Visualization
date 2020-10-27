7c5c3a59d88a99d85bd27a8b9dc3528cc133aa99
Damian <damian.jahn@icloud.com>
2020-10-27 16:11:03 +0100

    Update bar_charts.py

--- bar_charts.py
+++ bar_charts.py
@@ -14,15 +14,29 @@ index = np.arange(col_count)
 k1 = plt.bar(index, korea_scores, bar_width,
              alpha=.4,
              label="Korea")
-c1 = plt.bar(index + 0.1, canada_scores, bar_width,
+c1 = plt.bar(index + 0.2, canada_scores, bar_width,
              alpha=.4,
              label="Canada")
-ch1 = plt.bar(index + 0.2, china_scores, bar_width,
-              alpha=.4,
+ch1 = plt.bar(index + 0.4, china_scores, bar_width,
+              alpha=.3,
               label="China")
-f1 = plt.bar(index + 0.3, france_scores, bar_width,
+f1 = plt.bar(index + 0.6, france_scores, bar_width,
              alpha=.4,
              label="France")
+
+
+def create_labels(data):
+    for item in data:
+        height = item.get_height()
+        plt.text(item.get_x() + item.get_width() / 2., height*1.05,
+                 '%d' % int(height),
+                 ha='center', va='bottom')
+
+
+create_labels(k1)
+create_labels(c1)
+create_labels(ch1)
+create_labels(f1)
 
 plt.ylabel("Mean score in PISA 2012")
 plt.xlabel("Subjects")
